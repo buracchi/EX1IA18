d053b76aeff3b1db33ab5445c1e3dc23161c70c5
docb115 <45599613+docb115@users.noreply.github.com>
2018-12-04 23:57:28 +0100

    Update SwagDictionary.py
    Fixed marginal stuff, fixed bugged search function

--- strutturaDati/SwagDictionary.py
+++ strutturaDati/SwagDictionary.py
@@ -1,16 +1,19 @@
-from strutturaDati.LinkedListAVLDictionaryHybrid import LinkedListAVLDictionaryHybrid
+from strutturaDati.linkedListAVLDictionaryHybrid import LinkedListAVLDictionaryHybrid
 
 class SwagDictionary:
 
     def __init__(self, max, min, b):
         self.r = 6
+        assert max > min, "il parametro max dev'essere maggiore del parametro min"
+        assert b > self.r, "il parametro b dev'essere maggiore di " + str(self.r)
+        assert not (max-min)%b, "max - min dev'essere un multiplo di b"
         self.max = max
         self.min = min
         self.b = b
         self.d = int(abs((max - min) / b))
         self.v = []
         for i in range(0, self.d+2):
-            self.v.append(LinkedListAVLDictionaryHybrid())
+            self.v.append(LinkedListAVLDictionaryHybrid(self.r))
 
     def getIndexFromKey(self, key):
         if key > self.max:
@@ -31,4 +34,4 @@ class SwagDictionary:
 
     def search(self, key):
         i = self.getIndexFromKey(key)
-        self.v[i].search(key)
+        return self.v[i].search(key)
